be50537872b66901510923fa755d287dfe91fdbf
Justin Winger <justinwinger4@gmail.com>
2020-04-09 17:21:15 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,7 +5,7 @@ class Scraper
   attr_accessor :name, :location, :profile_url
 
   def self.scrape_index_page(index_url)
-    
+    Scraper.scrape_index_page(index_url)
   end
 
   def self.scrape_profile_page(profile_url)
